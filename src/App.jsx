0c4e51e9c5f7dce174d494ad9bf6c509548d6135
ash730xo <ash730@gmail.com>
2023-11-14 13:55:42 -0500

    correct array dependency when count increments

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import React, { useEffect, useState } from "react"
 
 export default function App() {
     const [starWarsData, setStarWarsData] = useState({})
-    const [count, setCount] = React.useState(0)
+    const [count, setCount] = React.useState(1)
 
 
 
     //Side Effects
     useEffect(() => {
-        console.log("effect function ran")
+        fetch(`https://swapi.dev/api/people/${count}`)
+        .then(res => res.json())
+        .then(data => setStarWarsData(data))
     }, [count])
 
-    }
+    
 
     return (
         <div>
-            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
             <h2>The count is {count}</h2>
             <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+
         </div>
     )
 }
